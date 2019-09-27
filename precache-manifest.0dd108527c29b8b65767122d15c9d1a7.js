4a749ac121907084a8a12ab11db6919d7a18baa3
sctveb <sctveb@gmail.com>
2019-09-27 18:00:53 +0900

    Updates

---
+++ precache-manifest.0dd108527c29b8b65767122d15c9d1a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ec009a1196ebd4bb29a850f611e240c",
+    "url": "/online-resume/index.html"
+  },
+  {
+    "revision": "86c1d63e7d33dcc33424",
+    "url": "/online-resume/static/css/main.c84b256a.chunk.css"
+  },
+  {
+    "revision": "5e76a29e991e362362c6",
+    "url": "/online-resume/static/js/2.77cbc593.chunk.js"
+  },
+  {
+    "revision": "86c1d63e7d33dcc33424",
+    "url": "/online-resume/static/js/main.560e4eaa.chunk.js"
+  },
+  {
+    "revision": "489aa18f5e75a3b5fcea",
+    "url": "/online-resume/static/js/runtime-main.e6849c06.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/online-resume/precache-manifest.dbaa45327421724bbc13fbf221deb7ae.js"
+  "/online-resume/precache-manifest.0dd108527c29b8b65767122d15c9d1a7.js"
 );
 
 self.addEventListener('message', (event) => {
